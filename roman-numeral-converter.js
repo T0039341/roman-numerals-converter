ac003a2c7e3e1f3be39722b0c57d07ddb737928a
t0039341 <t.hojeiri@gmail.com>
2021-11-08 17:06:57 +0200

    Add roman numeral converter file

---
+++ roman-numeral-converter.js
@@ -0,0 +1,41 @@
+/*
+Translate the number to roman numerals
+*/
+
+
+function run(n) {
+    /*
+     * Write your code below; return type and arguments should be according to the problem\'s requirements
+     */
+    let str = '';
+    const romNumsObj = {
+        1: 'I',
+        4: 'IV',
+        5: 'V',
+        9: 'IX',
+        10: 'X',
+        40: 'XL',
+        50: 'L',
+        90: 'XC',
+        100: 'C',
+        400: 'CD',
+        500: 'D',
+        900: 'CM',
+        1000: 'M'
+    }
+
+    const decKeys = Object.keys(romNumsObj).reverse();
+
+    decKeys.forEach(key => {
+        while (key <= n) {
+            str += romNumsObj[key];
+            console.log(str)
+            n -= key;
+            console.log(n)
+        }
+    });
+
+    return str;
+
+
+}
